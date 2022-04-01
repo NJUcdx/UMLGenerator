902c0f47567842199e200c34d7c3bf0cc349f7e7
chendongxiao <MF21320014@smail.nju.edu.cn>
2022-04-01 15:07:01 +0800

    Initial commit

---
+++ src/main/java/com/example/umlgenerator/UmlGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.umlgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmlGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/umlgenerator/UmlGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umlgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
